31d1d8090a79b094991a3fac9820984d15eaa6f6
tausif900 <ansaritausif9268@gmail.com>
2026-06-09 08:17:35 +0530

    Classroom

--- springbootbasics/src/main/java/com/study/springbootbasics/controller/SecondController.java
+++ springbootbasics/src/main/java/com/study/springbootbasics/controller/SecondController.java
@@ -3,7 +3,12 @@ package com.study.springbootbasics.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.springbootbasics.entities.Product;
@@ -16,7 +21,7 @@ public class SecondController {
 		return new Product(101, "Laptop", 45000);
 	}
 
-//	Now if we want to more products so we store in an ArrayList
+//	Now if we want to add more products so we store in an ArrayList
 
 	@GetMapping("/products")
 	public List<Product> gellAllProducts() {
@@ -25,5 +30,26 @@ public class SecondController {
 		products.add(new Product(102, "Tablet", 25000));
 		products.add(new Product(103, "Computer", 50000));
 		return products;
+	}
+
+//	This is a Simple way
+//	Adding data to the Server is 'POST'.
+//	@PostMapping("/add-product")
+//	public String addProduct(@RequestBody Product product) { // Here Post-man me post kiyahua data is product type me
+//																// store horaha hai with the help of requestBody annotation
+//		return "Product added successfully";
+//	}
+
+//	Response String type ka hai...
+//	isko me control krsktahu, status code bataskta, ise customization kehte hai..
+	@PostMapping("/add-product")
+	public ResponseEntity<String> addProduct(@RequestBody Product product) { // Here Post-man me post kiyahua data is
+																				// product type me store horaha hai with
+																				// the help of requestBody annotation
+		return new ResponseEntity<String>("Product added", HttpStatus.CREATED); // ResponseEntity ka obj return krna
+																				// rehta hai that has two parameters
+																				// ,one is what reponse we want we write
+																				// there and then which status Code we
+																				// want
 	}
 }
